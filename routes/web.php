bb69378814e54fba42287899fe0e55125f1cede7
Faizrashid Daffa <faizdaffanaut@gmail.com>
2026-05-21 19:34:32 +0700

    pokoe tampilan

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/login', function () {
 Route::get('/cart', function () {
     return view('cart');
 })->name('cart');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
