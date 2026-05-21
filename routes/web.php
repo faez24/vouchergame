df73a4fe9ba1892c10511cc1ea14c8ced00503e8
willynwr <wildanwhat@gmail.com>
2026-05-21 19:51:59 +0700

    games

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/checkout', function () {
 Route::get('/voucher', function () {
     return view('voucher');
 })->name('voucher');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
